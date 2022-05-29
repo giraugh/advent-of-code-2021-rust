858e66dc0108c64f59e0dd093722442d6e1588c7
Ewan Breakey <ebreakey@gmail.com>
2022-05-29 20:47:00 +1000

    Add solution for day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,23 +7,45 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 const SAMPLE_DEPTHS: [isize; 10] = [ 99, 200, 208, 210, 200, 207, 240, 269, 260, 263 ];
-const SAMPLE_INCREASES: usize = 7;
+const SAMPLE_INCREASES_PT1: usize = 7;
+const SAMPLE_INCREASES_PT2: usize = 5;
 const PUZZLE_INPUT_PATH: &str = "./puzzle_input.txt";
 
 fn main() -> Result<(), std::io::Error> {
-    // Test using sample data
-    let sample_increases = count_increases(&SAMPLE_DEPTHS);
-    println!("Sample increases: {}", sample_increases);
-    assert_eq!(sample_increases, SAMPLE_INCREASES);
-
     // Read input file
     let puzzle_input_file = File::open(PUZZLE_INPUT_PATH)?;
     let puzzle_depths: Vec<isize> = BufReader::new(puzzle_input_file)
         .lines().map(|l| l.unwrap().parse().unwrap()).collect();
 
-    // Test using puzzle input
+    part_1(&puzzle_depths)?;
+    part_2(&puzzle_depths)
+}
+
+fn part_1(puzzle_depths: &[isize]) -> Result<(), std::io::Error> {
+    // Test using sample data
+    let sample_increases = count_increases(&SAMPLE_DEPTHS);
+    println!("[PT1] Sample increases: {}", sample_increases);
+    assert_eq!(sample_increases, SAMPLE_INCREASES_PT1);
+
+    // Run on puzzle input
     let puzzle_increases = count_increases(&puzzle_depths);
-    println!("Puzzle increases: {}", puzzle_increases);
+    println!("[PT1] Puzzle increases: {}", puzzle_increases);
+
+    Ok(())
+}
+
+fn part_2(puzzle_depths: &[isize]) -> Result<(), std::io::Error> {
+    // Test using sample data
+    let sample_windows = sum_windows(&SAMPLE_DEPTHS, 3);
+    let sample_increases = count_increases(&sample_windows);
+    println!("[PT2] Sample increases: {}", sample_increases);
+    assert_eq!(sample_windows.len(), 8);
+    assert_eq!(sample_increases, SAMPLE_INCREASES_PT2);
+
+    // Run on puzzle input
+    let puzzle_windows = sum_windows(puzzle_depths, 3);
+    let puzzle_increases = count_increases(&puzzle_windows);
+    println!("[PT2] Puzzle increases: {}", puzzle_increases);
 
     Ok(())
 }
@@ -34,4 +56,11 @@ fn count_increases(depths: &[isize]) -> usize {
         .zip(depths.iter().skip(1))
         .map(|(depth, next_depth)| (depth < next_depth) as usize)
         .sum()
+}
+
+// Sum elements in sliding windows
+fn sum_windows(depths: &[isize], window_size: usize) -> Vec<isize> {
+   (0..=depths.len() - window_size)
+        .map(|i| depths[i..i+window_size].iter().sum())
+        .collect()
 }
